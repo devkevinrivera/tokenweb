11386fd67d744c2788a922762e74e74975de6567
adri villarreal <devkevinrivera@gmail.com>
2022-12-16 14:14:48 +0100

    .

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon } from 'semantic-ui-react';
 
 const HomeBanner = () => {
     return (
@@ -8,13 +8,14 @@ const HomeBanner = () => {
                 <Grid.Row>
                     <Grid.Column computer={16} className='home-banner__content'>
                         <Header as="h2">
-                            ESTUDIO DE DISEÑO <span>WEB</span> Y AGENCIA DE MARKETING DIGITAL
+                            ESTUDIO DE DISEÑO <span>WEB</span> Y DESARROLLO DE SOFTWARE
                         </Header>
                         <Header as="h3">
-                            Mejorar tu visibilidad en internet con la estrategia digital más adecuada para tu proyecto.
+                            Un único proveedor de servicios digitales para tu proyecto online.
                         </Header>
                         <Button className='button-home'>
-                            Si, me interesa
+                            <Icon name="terminal" />
+                            Me interesa
                         </Button>
                     </Grid.Column>
                 </Grid.Row>
