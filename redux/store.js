c8a1f15612fe57233b6d24f49c96f14f7ab57217
MacBook Air <devkevinrivera@gmail.com>
2022-12-21 13:49:34 +0100

    .

---
+++ redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { configSlice } from './slices/config'
+
+export const store = configureStore({
+  reducer: {
+    configReducer: configSlice
+  },
+})
