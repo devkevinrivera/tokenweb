c473a3d00b208c10d2b38b1dc84db9a2ee47232a
Kevin Rivera <devkevinrivera@gmail.com>
2023-03-24 00:50:40 +0100

    [ADD] -> Add layer

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,14 @@ import ContactSimple from '../components/ContactSimple'
 import TokenFooter from '../components/TokenFooter'
 import MenuToken from '../components/MenuToken'
 import { useSelector } from 'react-redux'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'G-R8SYVSLXY9'
+};
+TagManager.initialize(tagManagerArgs);
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   const showMenu = useSelector(state => state.app.showMenu);
   
@@ -23,6 +28,13 @@ export default function Home() {
     <section className='container-tokenweb'>
       <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R8SYVSLXY9"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', '');
+        </script>
         <title>TOKEN: Estudio de diseño web y desarrollo de software</title>
         <meta name="description" content="En nuestra agencia de diseño de software, creamos soluciones personalizadas y eficientes para llevar tu negocio al siguiente nivel. Nuestro equipo de expertos se enfoca en ofrecer productos de calidad, con una gran atención al detalle y una experiencia de usuario excepcional." />
         <meta name="keywords" content="Diseño web personalizado,Diseño web responsivo,Experiencia de usuario,Diseño de interfaz de usuario,Diseño de páginas web corporativas,Diseño de páginas web de comercio electrónico,Optimización para motores de búsqueda (SEO),Optimización para motores de búsqueda (SEO)"></meta>
